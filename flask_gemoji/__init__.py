1f97910376211c7fa7446ee0c61e5ad72102d58e
Maximiliano Rios <griosflores@kooggis.com>
2014-05-19 18:10:36 -0300

    [#UPDATE] Added methods to translate and get emoji from unicode
    Including first json emoji map. Not finished yet

--- flask_gemoji/__init__.py
+++ flask_gemoji/__init__.py
@@ -1,5 +1,6 @@
 import os
 import pkg_resources
+import json
 
 from flask import Blueprint, url_for, Markup
 
@@ -14,12 +15,20 @@ class Gemoji(object):
         names = map(lambda s: os.path.splitext(s)[0],
                     filter(lambda s: s.endswith('.png'),
                            os.listdir(gemoji_images_path)))
-
+        gemoji_files_path =  (pkg_resources
+            .resource_filename(__package__, 'static/files'))
+        gemoji_map = "%s/%s" % tuple([gemoji_files_path, "emojimap.json"])
+        f = open(gemoji_map, "r")
+        json_data = json.load(f, encoding="UTF8")
+        self.char_map = {}
+        for mapping in json_data["map"]:
+            self.char_map[mapping["key"]] = mapping["value"]
         gemoji = Blueprint('gemoji', __name__, static_folder='static')
         app.register_blueprint(gemoji, url_prefix='/gemoji')
 
         @app.template_filter('gemoji')
         def gemoji_filter(s, height='auto'):
+            s = self.replace_unicode(s)
             splits = s.split(':')
             splits_len = len(splits)
             out = ''
@@ -39,3 +48,18 @@ class Gemoji(object):
                     out += w
 
             return out
+
+
+    # This feature translates unicode characters into emoji strings
+    @classmethod
+    def name_for(self, character):
+        if character in self.char_map:
+            return self.char_map[character]
+        else:
+            return character
+
+    @classmethod
+    def replace_unicode(self, s):
+        for key in self.char_map.keys():
+            s = s.replace(key, self.char_map[key])
+        return s
